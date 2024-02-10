fb5abc05dfe243a3b7f6eb35fb369d0b8cbf299a
monika <moniravi31@gmail.com>
2024-02-09 23:02:12 -0500

    End of sprint1. API created

---
+++ api/models/Donation.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create schema
+const DonationSchema = new Schema({
+    campaign_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'campaigns',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+mongoose.model('donations', DonationSchema);
